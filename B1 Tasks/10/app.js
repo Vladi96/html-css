15997347da21e767e7872cbe73276effdde3f4cd
Vladi96 <vladika96@abv.bg>
2019-03-30 16:57:35 +0200

    .

--- B1 Tasks/10/app.js
+++ B1 Tasks/10/app.js
@@ -1,25 +1,25 @@
-$(()=>{
-    const maxCount = 3
-    let myImg = $(".my-img")
-    let count = 1 
+$(() => {
+    const maxCount = 3;
+    let myImg = $(".my-img");
+    let count = 1;
 
-    $(".next-img").click((event)=>{
-        event.preventDefault()
-        count++
-    
-        if ( count > maxCount){
-            count = 1
+    $(".next-img").click((event) => {
+        event.preventDefault();
+        count++;
+
+        if (count > maxCount) {
+            count = 1;
         }
-        myImg.attr('src',`css/images/banner-${count}.png`)
-    })
+        myImg.attr('src', `css/images/banner-${count}.png`);
+    });
 
-    $(".previous-img").click((event)=>{
-        event.preventDefault()
-        count--
+    $(".previous-img").click((event) => {
+        event.preventDefault();
+        count--;
 
-        if (count < 1 ){
-            count = maxCount
+        if (count < 1) {
+            count = maxCount;
         }
-        myImg.attr('src', `css/images/banner-${count}.png`)
-    })
-})()
+        myImg.attr('src', `css/images/banner-${count}.png`);
+    });
+})();
